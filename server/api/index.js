0a98a6968738f0451350f347f1ad84891807b187
Navdeep Singh <navdeep.er@gmail.com>
2021-06-11 16:32:36 +0530

    Update

--- server/api/index.js
+++ server/api/index.js
@@ -3,9 +3,6 @@ var app = express();
 var router = express.Router();
 var controller = require("./controller");
 
-//This is an API to get all users from Users table
-//The http/s URL of this API will be  "hostname|IP/api/getUsers" e.g "http://mywebapp.com/api/getUsers"
-
 router.get("/status", controller.getApiStatus);
 router.get("/urls", controller.getUrls);
 
